8b3185b67080566713b0197c214ce708122ae31e
Erwan31 <spilmonterwan@gmail.com>
2020-08-29 13:06:50 +0100

    Display playlist tracks+thumbnails

--- client/src/Components/tracklist.js
+++ client/src/Components/tracklist.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Table } from 'reactstrap';
 
 class TrackList extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            tracks: []
+            tracks: [],
+            filteredTracks: [],
         };
     }
 
@@ -20,19 +22,45 @@ class TrackList extends Component {
 
         const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {headers: headerContent});
         const data = await response.data;
-        console.log('tracklist', data);
+        
+        this.setState( { tracks: data.items });
+
            
     }
 
     render() { 
 
         console.log('render tracklist', this.props);
+        console.log('tracks', this.state.tracks);
+        const tracks = this.state.tracks;
 
         // Reactstrap table with up to a 100 songs displaying the album+title+...
         return ( 
-            <div>
-                Tracklist
-            </div>
+            <Table hover>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                {
+                    tracks.map( (track, i) => 
+                    <tr key={i}>
+                        <th scope="row" className="albumThumbnail">
+                            <img src={track.track.album.images[1].url}></img>
+                        </th>
+                        <td>{track.track.name}</td>
+                        <td>{track.track.artists[0].name}</td>
+                        <td>{track.track.album.name}</td>
+                    </tr>
+                    )
+                }
+            </tbody>
+          </Table>
          );
     }
 }

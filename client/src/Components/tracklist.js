071847fd24a38c44a2a6d9365e9437e57bc7b375
Erwan31 <spilmonterwan@gmail.com>
2020-08-30 13:49:34 +0100

    Reordering in fucntion of chosen criteria + pb

--- client/src/Components/tracklist.js
+++ client/src/Components/tracklist.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Table, Button, ButtonGroup } from 'reactstrap';
 
+const CRITERIA = ["danceability", "energy", "valence"];
+
 class TrackList extends Component {
     constructor(props){
         super(props);
 
         this.state = {
             tracks: [],
-            filteredTracks: [],
             audio_features: [],
+            tracksFeatures: [],
+            filteredTracksFeatures: [],
             rSelected: null,
             rDirectionInit: ["desc", "desc", "desc"],
             rDirection: ["desc", "desc", "desc"]
@@ -17,6 +20,8 @@ class TrackList extends Component {
     }
 
     async componentDidMount() {
+        console.log('compo did it');
+
         // Get up to 100 tracks from playlist 
         const token = this.props.playlistInfo.token;
         const playlistID = this.props.playlistInfo.selectedPlaylist;
@@ -43,45 +48,54 @@ class TrackList extends Component {
            
         this.setState({ audio_features:  data1.audio_features});
 
+
         if( this.state.audio_features.length > 0 ){
 
             const tracks = this.state.tracks;
             const audio_features = this.state.audio_features;
 
-            // Filter tracks based on a parameter
-            const filtered_features = this.sortByAscCriteria(audio_features, "danceability");
-            const filtered_ids = filtered_features.map( (track) =>
+             
+            // No Filter tracks based on a parameter
+            //const filtered_features = this.sortByAscCriteria(audio_features, "danceability");
+            const filtered_ids = audio_features.map( (track) =>
                                     track.id);
-            const filteredTracks = filtered_ids.map( id => {
+            const filteredTracksFeatures = filtered_ids.map( id => {
                 for( let i = 0; i < tracks.length; i++ ){
                     //console.log( id, tracks[i].track.id);
-                    if( id === tracks[i].track.id) return tracks[i].track;
+                    if( id === tracks[i].track.id) return [tracks[i].track, audio_features[i]];
                 }
             });
 
-            console.log('filteredTracks', filteredTracks);
-            this.setState( { filteredTracks });
+            console.log('filteredTracks', filteredTracksFeatures);
+            this.setState( { filteredTracksFeatures, tracksFeatures: filteredTracksFeatures });
         }
     }
 
     sortByAscCriteria = (arr, parameter) => {
-        const sorted = arr.sort( ( a, b) =>
-        a[parameter] - b[parameter]);
-
-        return sorted
+        const sorted = arr.sort( ( a, b) =>{
+            console.log(a[1][parameter])
+            return a[1][parameter] - b[1][parameter]
+        });
+        return sorted;
     }
 
     sortByDescCriteria = (arr, parameter) => {
         const sorted = arr.sort( ( a, b) =>
         b[parameter] - a[parameter]);
+        return sorted;
+    }
 
-        return sorted
+    reverseOrder = (arr) => {
+        const sorted = arr.sort( ( a, b) => -1);
     }
 
     // Radio Buttons
     onCheckboxBtnClick = (selected) => {
         let rDirection = this.state.rDirection;
         const selectedDirection = rDirection[selected];
+        let  filteredTracksFeatures = this.state.filteredTracksFeatures;
+        const tracksFeatures = this.state.tracksFeatures;
+        const rSelected = this.state.rSelected;
 
         // Reset state by spreading init state
         rDirection = [...this.state.rDirectionInit];
@@ -91,14 +105,27 @@ class TrackList extends Component {
             rDirection[selected] = "asc";
         }
         else rDirection[selected] = "desc";
+        
+        if( selected !== null ){
+            console.log('features', filteredTracksFeatures);
+            filteredTracksFeatures = this.sortByAscCriteria( filteredTracksFeatures, CRITERIA[rSelected]);
+            console.log('selection', CRITERIA[rSelected]);
+            this.setState({ filteredTracksFeatures });
+        }
+        else {
+            filteredTracksFeatures = tracksFeatures;
+            console.log('no selection');
+        }
 
         this.setState({ rSelected: selected, rDirection });
+
     }
 
 
     render() { 
-        const filteredTracks = this.state.filteredTracks;
-        console.log(this.state.rSelected, this.state.rDirection);
+        const  filteredTracksFeatures = this.state.filteredTracksFeatures;
+
+        console.log('render');
 
         // Reactstrap table with up to a 100 songs displaying the album+title+...
         return (
@@ -126,14 +153,14 @@ class TrackList extends Component {
 
                         <tbody>
                             {
-                            filteredTracks && filteredTracks.map( (track, i) => 
+                            filteredTracksFeatures[0] && filteredTracksFeatures.map( (track, i) => 
                                 <tr key={i}>
                                     <th scope="row">
-                                        <img className="albumThumbnail" src={track.album.images[1].url || track.album.images[0].url}></img>
+                                        <img className="albumThumbnail" src={track[0].album.images[1].url || track[0].album.images[0].url}></img>
                                     </th>
-                                    <td>{track.name}</td>
-                                    <td>{track.artists[0].name}</td>
-                                    <td>{track.album.name}</td>
+                                    <td>{track[0].name}</td>
+                                    <td>{track[0].artists[0].name}</td>
+                                    <td>{track[0].album.name}</td>
                                 </tr>
                                 )
                             }

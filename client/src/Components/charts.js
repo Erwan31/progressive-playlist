5cf468997c26ef69555771833a0645a3ff84a1c1
Erwan31 <spilmonterwan@gmail.com>
2020-08-31 13:36:19 +0100

    Flex display charts, issues CSS and overflow

--- client/src/Components/charts.js
+++ client/src/Components/charts.js
@@ -6,42 +6,41 @@ const Charts = ({tracksFeatures}) => {
 
     const features = tracksFeatures.map( track => track[1]);
     const danceability = features.map( track => track.danceability);
-    const danceLength = danceability.length;
-    const labels = danceability.map( () => "");
-    console.log(danceLength, danceability);
-    const data = {
-        labels: labels,
-        datasets: [
-          {
-            label: 'My First dataset',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: danceability
-          }
-        ]
-      };
+    const energy = features.map( track => track.energy);
+    const valence = features.map( track => track.valence);
+    const length = features.length;
+    const labels = features.map( () => "");
+    console.log(length, danceability);
 
-    const chartData = {
-        labels: [],
-        datasets: [{
-            data: [{x:0.65}, {x:0.59}, {x:0.80}, {x:0.81}, {x:0.56}, {x:0.55}, {x:0.40}]
-        }]
-    }
+
+    const data = (arr, label, color) =>{
+        return {
+            labels: labels,
+            datasets: [
+            {
+                label: label,
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: color,
+                borderColor: color,
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: color,
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: color,
+                pointHoverBorderColor: color,
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: arr
+            }
+            ]
+        }
+    };
 
     const options = {
         responsive: true,
@@ -66,7 +65,7 @@ const Charts = ({tracksFeatures}) => {
                 },
                 ticks: {
                     min: 0,
-                    max: danceLength
+                    max: length
                 },
             }],
             yAxes: [{
@@ -84,11 +83,18 @@ const Charts = ({tracksFeatures}) => {
     }
 
     return ( 
-        <div className="chart">
+        <div className="charts">
+            <Line
+                className="chart" 
+                data={data(danceability, "Danceability", "green")}
+            />
             <Line 
-                data={data}
-                width={400}
-                height={400}
+                className="chart"
+                data={data(energy, "Energy", "blue")}
+            />
+            <Line 
+                className="chart"
+                data={data(valence, "Mood", "Yellow")}
             />
         </div>
      );

7898b10a939b044532a2d0a3140d372ad1936e0c
Erwan31 <spilmonterwan@gmail.com>
2020-10-06 17:22:12 +0100

    Access to collaborative playlists also now

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "4f5288c7f7ca4f8e995e861a262eb902";
-export const redirectUri =  "https://playlits.herokuapp.com/redirect"; //"http://localhost:3000/redirect";
+export const redirectUri =  "http://localhost:3000/redirect"; //"https://playlits.herokuapp.com/redirect"; 
 export const scopes = [
-    "user-top-read",
-    "user-read-currently-playing",
-    "user-read-playback-state",
+    //"user-top-read",
+    //"user-read-currently-playing",
+    //"user-read-playback-state",
     "playlist-read-private",    // access playlists of the user
+    "playlist-read-collaborative", // access collaborative playlist of the user
     "playlist-modify-private",  // create new playlist
     "playlist-modify-public",
- //   "playlist-read-collaborative", // access collaborative playlist of the user
-    "streaming" // Web Playback SDK in the case 
+    //"streaming" // Web Playback SDK in the case 
 ]; 
 
